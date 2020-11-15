33935eeb5fca070ed756599ecab43bd6f81e1d05
lestad <j.v.gospodinov@gmail.com>
2020-11-15 14:09:53 +0200

    removing some stuff adding some APIs

--- src/main/java/uoi_project/BaseInformationController.java
+++ src/main/java/uoi_project/BaseInformationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
@@ -141,4 +142,305 @@ public class BaseInformationController {
         return uoiNode;
     }
 
+    @GetMapping("/demoNodes")
+    public String executeDemoNode(){
+        uoiRepository.deleteAll();
+        demoNodes(uoiRepository);
+        return "Success";
+    }
+
+    @GetMapping("/demoNodesCombineTwoRooms")
+    public String executeDemoNodeCombineTwoRooms(){
+        uoiRepository.deleteAll();
+        demoNodesCombineTwoRooms(uoiRepository);
+        return "<html><body>" + "<img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0300ffb5-857b-40a1-a2f1-f377d78b2952/d2ykrux-efdd3e45-aefd-4bac-8506-61a147be6127.jpg/v1/fill/w_900,h_1352,q_75,strp/kartinka_by_overthecuckoo_d2ykrux-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMzUyIiwicGF0aCI6IlwvZlwvMDMwMGZmYjUtODU3Yi00MGExLWEyZjEtZjM3N2Q3OGIyOTUyXC9kMnlrcnV4LWVmZGQzZTQ1LWFlZmQtNGJhYy04NTA2LTYxYTE0N2JlNjEyNy5qcGciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.5my9yERBylqyGfyKcJdL2pUG5HVJZz78xdbNmkOqqbw'/> " +
+                "</body></html>";
+    }
+
+    @GetMapping("/demoNodesAddANewRoom")
+    public String executeDemoNodeAddANewRoom(){
+        uoiRepository.deleteAll();
+        demoNodesAddANewRoom(uoiRepository);
+        return "<html><body>" + "<img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0300ffb5-857b-40a1-a2f1-f377d78b2952/d2ykrux-efdd3e45-aefd-4bac-8506-61a147be6127.jpg/v1/fill/w_900,h_1352,q_75,strp/kartinka_by_overthecuckoo_d2ykrux-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD0xMzUyIiwicGF0aCI6IlwvZlwvMDMwMGZmYjUtODU3Yi00MGExLWEyZjEtZjM3N2Q3OGIyOTUyXC9kMnlrcnV4LWVmZGQzZTQ1LWFlZmQtNGJhYy04NTA2LTYxYTE0N2JlNjEyNy5qcGciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.5my9yERBylqyGfyKcJdL2pUG5HVJZz78xdbNmkOqqbw'/> " +
+                "</body></html>";
+    }
+
+
+    public void demoNodes(UOIRepository uoiRepository){
+        List<UOINode> nodes = new ArrayList<UOINode>();
+
+        //walls
+        UOINode wall1 = new UOINode(LEVEL.WALL);
+        UOINode wall2 = new UOINode(LEVEL.WALL);
+        UOINode wall3 = new UOINode(LEVEL.WALL);
+        UOINode wall4 = new UOINode(LEVEL.WALL);
+
+        nodes.add(wall1);
+        nodes.add(wall2);
+        nodes.add(wall3);
+        nodes.add(wall4);
+
+        UOINode room1 = new UOINode(LEVEL.ROOM);
+
+        nodes.add(room1);
+
+        UOINode wall5 = new UOINode(LEVEL.WALL);
+        UOINode wall6 = new UOINode(LEVEL.WALL);
+        UOINode wall7 = new UOINode(LEVEL.WALL);
+        UOINode wall8 = new UOINode(LEVEL.WALL);
+
+        nodes.add(wall5);
+        nodes.add(wall6);
+        nodes.add(wall7);
+        nodes.add(wall8);
+
+        UOINode room2 = new UOINode(LEVEL.ROOM);
+
+        nodes.add(room2);
+
+        UOINode unit1 = new UOINode(LEVEL.UNIT);
+
+        nodes.add(unit1);
+
+        uoiRepository.saveAll(nodes);
+
+        //adding the walls to a room1
+        wall1.partOf(room1);
+        wall2.partOf(room1);
+        wall3.partOf(room1);
+        wall4.partOf(room1);
+
+        uoiRepository.saveAll(nodes);
+
+        //room1 is consisting of the walls
+        room1.consistsOf(wall1);
+        room1.consistsOf(wall2);
+        room1.consistsOf(wall3);
+        room1.consistsOf(wall4);
+
+        uoiRepository.saveAll(nodes);
+        //adding the walls to a room2
+        wall5.partOf(room2);
+        wall6.partOf(room2);
+        wall7.partOf(room2);
+        wall8.partOf(room2);
+
+        uoiRepository.saveAll(nodes);
+
+        //room2 is consisting of the walls
+        room2.consistsOf(wall5);
+        room2.consistsOf(wall6);
+        room2.consistsOf(wall7);
+        room2.consistsOf(wall8);
+
+        uoiRepository.saveAll(nodes);
+
+        //adding the rooms to unit 1
+        room1.partOf(unit1);
+        room2.partOf(unit1);
+
+        uoiRepository.saveAll(nodes);
+
+        //the unit is consisted of
+        unit1.consistsOf(room1);
+        unit1.consistsOf(room2);
+
+        uoiRepository.saveAll(nodes);
+    }
+
+    public void demoNodesCombineTwoRooms(UOIRepository uoiRepository){
+        List<UOINode> nodes = new ArrayList<UOINode>();
+
+        //walls
+        UOINode wall1 = new UOINode(LEVEL.WALL);
+        UOINode wall2 = new UOINode(LEVEL.WALL);
+        UOINode wall3 = new UOINode(LEVEL.WALL);
+        UOINode wall4 = new UOINode(LEVEL.WALL);
+
+        nodes.add(wall1);
+        nodes.add(wall2);
+        nodes.add(wall3);
+        nodes.add(wall4);
+
+        UOINode room1 = new UOINode(LEVEL.ROOM);
+
+        nodes.add(room1);
+
+        UOINode wall5 = new UOINode(LEVEL.WALL);
+        UOINode wall6 = new UOINode(LEVEL.WALL);
+        UOINode wall7 = new UOINode(LEVEL.WALL);
+        UOINode wall8 = new UOINode(LEVEL.WALL);
+
+        nodes.add(wall5);
+        nodes.add(wall6);
+        nodes.add(wall7);
+        nodes.add(wall8);
+
+        UOINode room2 = new UOINode(LEVEL.ROOM);
+
+        nodes.add(room2);
+
+        // adding the new room that combines the two initial rooms
+        // combining two rooms
+        UOINode roomCombined = new UOINode(LEVEL.ROOM);
+        nodes.add(roomCombined);
+
+        UOINode unit1 = new UOINode(LEVEL.UNIT);
+
+        nodes.add(unit1);
+
+        uoiRepository.saveAll(nodes);
+
+
+        //adding the walls to a room1
+        wall1.partOf(roomCombined);
+        wall2.partOf(roomCombined);
+        wall3.partOf(roomCombined);
+        wall4.partOf(roomCombined);
+
+        uoiRepository.saveAll(nodes);
+
+        //room1 is consisting of the walls
+        roomCombined.consistsOf(wall1);
+        roomCombined.consistsOf(wall2);
+        roomCombined.consistsOf(wall3);
+        roomCombined.consistsOf(wall4);
+
+        uoiRepository.saveAll(nodes);
+        //adding the walls to a room2
+        wall5.partOf(roomCombined);
+        wall6.partOf(roomCombined);
+
+        uoiRepository.saveAll(nodes);
+
+        //room2 is consisting of the walls
+        roomCombined.consistsOf(wall5);
+        roomCombined.consistsOf(wall6);
+
+        uoiRepository.saveAll(nodes);
+
+        //adding the room to unit 1
+        roomCombined.partOf(unit1);
+
+        uoiRepository.saveAll(nodes);
+
+        //the unit is consisted of
+        unit1.consistsOf(roomCombined);
+        uoiRepository.saveAll(nodes);
+
+        //History of the rooms
+        room1.historyOf(roomCombined);
+        room2.historyOf(roomCombined);
+        uoiRepository.saveAll(nodes);
+    }
+
+    public void demoNodesAddANewRoom(UOIRepository uoiRepository){
+        List<UOINode> nodes = new ArrayList<UOINode>();
+
+        //walls
+        UOINode wall1 = new UOINode(LEVEL.WALL);
+        UOINode wall2 = new UOINode(LEVEL.WALL);
+        UOINode wall3 = new UOINode(LEVEL.WALL);
+        UOINode wall4 = new UOINode(LEVEL.WALL);
+
+        nodes.add(wall1);
+        nodes.add(wall2);
+        nodes.add(wall3);
+        nodes.add(wall4);
+
+        UOINode room1 = new UOINode(LEVEL.ROOM);
+
+        nodes.add(room1);
+
+        UOINode wall5 = new UOINode(LEVEL.WALL);
+        UOINode wall6 = new UOINode(LEVEL.WALL);
+        UOINode wall7 = new UOINode(LEVEL.WALL);
+        UOINode wall8 = new UOINode(LEVEL.WALL);
+
+        nodes.add(wall5);
+        nodes.add(wall6);
+        nodes.add(wall7);
+        nodes.add(wall8);
+
+        UOINode room2 = new UOINode(LEVEL.ROOM);
+
+        nodes.add(room2);
+
+        UOINode wall9 = new UOINode(LEVEL.WALL);
+        UOINode wall10 = new UOINode(LEVEL.WALL);
+
+        nodes.add(wall9);
+        nodes.add(wall10);
+
+        UOINode room3 = new UOINode(LEVEL.ROOM);
+
+        nodes.add(room3);
+
+        UOINode unit1 = new UOINode(LEVEL.UNIT);
+
+        nodes.add(unit1);
+
+        uoiRepository.saveAll(nodes);
+
+        //adding the walls to a room1
+        wall1.partOf(room1);
+        wall2.partOf(room1);
+        wall3.partOf(room1);
+        wall4.partOf(room1);
+
+        uoiRepository.saveAll(nodes);
+
+        //room1 is consisting of the walls
+        room1.consistsOf(wall1);
+        room1.consistsOf(wall2);
+        room1.consistsOf(wall3);
+        room1.consistsOf(wall4);
+
+        uoiRepository.saveAll(nodes);
+        //adding the walls to a room2
+        wall5.partOf(room2);
+        wall6.partOf(room2);
+        wall7.partOf(room2);
+        wall8.partOf(room2);
+
+        uoiRepository.saveAll(nodes);
+
+        //room2 is consisting of the walls
+        room2.consistsOf(wall5);
+        room2.consistsOf(wall6);
+        room2.consistsOf(wall7);
+        room2.consistsOf(wall8);
+
+        uoiRepository.saveAll(nodes);
+
+        //adding the walls to a room3
+        wall4.partOf(room3);
+        wall8.partOf(room3);
+        wall9.partOf(room3);
+        wall10.partOf(room3);
+
+        uoiRepository.saveAll(nodes);
+
+        //room2 is consisting of the walls
+        room3.consistsOf(wall4);
+        room3.consistsOf(wall8);
+        room3.consistsOf(wall9);
+        room3.consistsOf(wall10);
+
+        uoiRepository.saveAll(nodes);
+
+        //adding the rooms to unit 1
+        room1.partOf(unit1);
+        room2.partOf(unit1);
+        room3.partOf(unit1);
+
+        uoiRepository.saveAll(nodes);
+
+        //the unit is consisted of
+        unit1.consistsOf(room1);
+        unit1.consistsOf(room2);
+        unit1.consistsOf(room3);
+
+        uoiRepository.saveAll(nodes);
+    }
 }
